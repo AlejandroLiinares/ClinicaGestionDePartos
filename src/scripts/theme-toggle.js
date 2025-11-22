f09bc7a8de0eacd1f8fb7c5db6f7572cd37a440f
AlejandroLiinares <alejandrolinaresnt@gmail.com>
2025-11-22 00:00:31 -0300

    Cambio de aspecto funcional y primer diseño listo

---
+++ src/scripts/theme-toggle.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("themeToggle");
+const icon = document.getElementById("themeIcon");
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("light");
+
+    // Cambia el icono sol/luna
+    if (document.body.classList.contains("light")) {
+        icon.src = "../assets/svg/moon.svg";
+    } else {
+        icon.src = "../assets/svg/sun.svg";
+    }
+});
