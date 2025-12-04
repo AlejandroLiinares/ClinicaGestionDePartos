435e3d0f86e8ead0ae0e43914283e3c5b95ff210
AlejandroLiinares <alejandrolinaresnt@gmail.com>
2025-12-04 15:31:26 -0300

    Navegación completa entre todos los modulos de Matrona junto con el tema osucro y blanco funcional en cualquier página

---
+++ src/scripts/sub-themes.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById("themeToggle");
+const icon = document.getElementById("themeIcon");
+
+// Actualizar icono según el estado actual
+function updateIcon() {
+    if (document.body.classList.contains("dark")) {
+        icon.src = "../../../assets/svg/sun.svg"; // Oscuro → Sol
+    } else {
+        icon.src = "../../../assets/svg/moon.svg"; // Claro → Luna
+    }
+}
+
+// Ejecutar cuando la página carga
+updateIcon();
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    updateIcon();
+});
